7507b8a9c1c5200f5653b5f36351175b144adf36
ahsanalikhan13 <aahsanalikhan.13@gmail.com>
2023-02-08 18:48:28 +0500

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Drawer,DrawerBody,Button,DrawerHeader,DrawerOverlay,DrawerContent,DrawerCloseButton, useDisclosure } from '@chakra-ui/react'
+import { Drawer,DrawerBody,Button,DrawerHeader,DrawerOverlay,DrawerContent,DrawerCloseButton, useDisclosure, VStack, HStack } from '@chakra-ui/react'
 import { Link } from 'react-router-dom';
 import {BiMenuAltLeft} from 'react-icons/bi';
 
 const Header = () => {
-
     const {isOpen, onOpen, onClose}= useDisclosure();
 
   return (
@@ -14,20 +13,60 @@ const Header = () => {
         pos={'fixed'} 
         top={'4'} 
         left={'4'} 
-        colorScheme={'facebook'} 
+        colorScheme={'whatsapp'} 
         p={'0'} 
         w={'10'} 
         h={'10'} 
         borderRadius={'full'}
         onClick={onOpen}
         >
-            <BiMenuAltLeft size={'20'}/>
+            <BiMenuAltLeft size={'20'} />
         </Button>
 
-        <Drawer isOpen={isOpen}>
-            <div>
-                <p>Hello</p>
-            </div>
+        <Drawer isOpen={isOpen} placement={'left'} onClose={onClose}>
+            <DrawerOverlay/>
+            <DrawerContent >
+                <DrawerCloseButton/>
+                <DrawerHeader>VIDEO COLLECTIONS</DrawerHeader>
+                
+                <DrawerBody >
+                    <VStack alignItems={'start'} padding={'8'} marginLeft={'-7'}>
+                        <Button onClick={onClose} variant={'ghost'} colorScheme={'whatsapp'}>
+                            <Link to={'/'}>Home</Link>
+                        </Button>
+
+                        <Button onClick={onClose} variant={'ghost'} colorScheme={'whatsapp'}>
+                            <Link to={'/videos'}>Videos</Link>
+                        </Button>
+
+                        <Button onClick={onClose} variant={'ghost'} colorScheme={'whatsapp'}>
+                            <Link to={'/videos?category=free'}>Free Videos</Link>
+                        </Button>
+
+                        <Button onClick={onClose} variant={'ghost'} colorScheme={'whatsapp'}>
+                            <Link to={'/upload'}>Upload Video</Link>
+                        </Button>
+                    </VStack>
+
+
+                    <HStack 
+                    pos={'absolute'} 
+                    bottom={'10'} 
+                    left={'0'}
+                    w={'full'}
+                    justifyContent={'space-evenly'}>
+                        <Button colorScheme={'whatsapp'} variant={'outline'}>
+                            <Link to={'/login'}>Log In</Link>
+                        </Button>
+
+                        <Button colorScheme={'whatsapp'} variant={'outline'}>
+                            <Link to={'/signup'}>Sign Up</Link>
+                        </Button>
+                    </HStack>
+
+
+                </DrawerBody>
+            </DrawerContent>
         </Drawer>
 
     </Link>
